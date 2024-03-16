df0cbfe3aa3a346be902bb614bdf948286aeb5fd
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-03-16 20:52:07 +0300

    init

---
+++ app/src/main/java/com/example/notes21/AddNoteFragment.kt
@@ -0,0 +1,65 @@
+package com.example.notes21
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.notes21.data.Group
+import com.example.notes21.data.Note
+import com.example.notes21.data.NotesService
+import com.example.notes21.databinding.FragmentAddNoteBinding
+import com.example.notes21.databinding.FragmentNotesBinding
+import java.time.LocalDate
+
+class AddNoteFragment : Fragment() {
+
+    private var binding: FragmentAddNoteBinding? = null
+    private lateinit var noteAdapter: NoteAdapter
+    private val notesService: NotesService
+        get() = (requireActivity().applicationContext as App).notesService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddNoteBinding.inflate(inflater, container, false)
+        noteAdapter = NoteAdapter()
+        val items = notesService.getItems()
+
+        binding?.let { binding ->
+            binding.addButton.setOnClickListener() {
+                val title = binding.dialogTitle.text.toString()
+                val text = binding.dialogText.text.toString()
+
+                val newItem = if (title.isNotEmpty()) {
+                    if (text.isNotEmpty()) {
+                        Note(title, text, LocalDate.now())
+                    } else Group(title)
+                } else null
+                newItem?.let { item ->
+                    notesService.addItem(item)
+                    noteAdapter.notifyItemInserted(items.size - 1)
+                }
+            }
+
+            binding.cancelButton.setOnClickListener() {
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+
+        }
+        return binding!!.root
+    }
+
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+}
---
+++ app/src/main/java/com/example/notes21/NotesFragment.kt
@@ -0,0 +1,76 @@
+package com.example.notes21
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notes21.data.ItemsListener
+import com.example.notes21.data.NotesService
+import com.example.notes21.databinding.FragmentNotesBinding
+
+class NotesFragment : Fragment() {
+    private lateinit var noteAdapter: NoteAdapter
+    private var bindingFragment: FragmentNotesBinding? = null
+
+    private val notesService: NotesService
+        get() = (requireActivity().applicationContext as App).notesService
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bindingFragment = FragmentNotesBinding.inflate(layoutInflater)
+        noteAdapter = NoteAdapter()
+
+        val recyclerView = bindingFragment?.notes
+
+        recyclerView?.adapter = noteAdapter
+        recyclerView?.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        bindingFragment?.addNoteButton?.setOnClickListener {
+            showAddNoteFragment()
+        }
+
+        val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback(notesService))
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+
+        notesService.addListener(itemsListener)
+        noteAdapter.items = notesService.getItems()
+        return bindingFragment!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        notesService.removeListener(itemsListener)
+        bindingFragment = null
+        super.onDestroyView()
+    }
+
+    private fun showAddNoteFragment() {
+        val addNoteFragment = AddNoteFragment()
+
+        //requireActivity() возвращает Activity, с которым связан текущий фрагмент
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, addNoteFragment)
+            .commit()
+    }
+
+    private val itemsListener: ItemsListener = {
+        noteAdapter.items = it
+    }
+}
